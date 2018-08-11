fdec8e6ea8115be370ded9c9032b5d8a764c36ef
santoshtechie9@gmail.com <September*09>
2018-08-11 13:04:23 -0700

    added  mortal and owned contracts

--- contracts/ProofOfOwnership.sol
+++ contracts/ProofOfOwnership.sol
@@ -1,25 +1,63 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.22;
 
-contract ProofOfOwnership {
 
-    struct Document {
-        address owner;
-        string ownerName;
-        string ownerEmail;
-        string docTimestamp;
-        string docHash;
+contract Owned {
+    
+    address public owner;
+    constructor() public {
+        owner = msg.sender;
     }
     
-    mapping( string => Document) documents; 
+}
 
-    function addNewDocument(string _ownerName, string _ownerEmail, string _docTimestamp, string _docHash) public returns(bool){
-        documents[_docHash] = Document(msg.sender,_ownerName,_ownerEmail,_docTimestamp,_docHash);
+contract Mortal is Owned {
+    
+    function kill() public {
+        if(msg.sender == owner){
+            selfdestruct(owner);
+        }
+        
+    }
+    
+}
+    
+contract ProofOfOwnership is Mortal {
+
+    struct Document {
+        string docHash;
+        string docTimestamp;
+        string currentOwnerName;
+        address currentOwnerAddress;
+        //address[] previousOwners;
+    }
+    
+    mapping( string => Document) public documents; 
+
+    function addDocument(string _docHash, string _docTimestamp, string _currentOwnerName) public returns(bool){
+       
+       //Input paramenters validation
+       //Document hash should not be empty and length should 256 bytes
+        require( 
+            bytes(_docHash).length > 0 && bytes(_docHash).length <= 256, 
+            "Hash length should be 256 bytes"
+            );
+           
+        documents[_docHash] = Document(_docHash, _docTimestamp, _currentOwnerName, msg.sender);
+        
         return(true);
     }
 
-    function getDocumentDetails(string _documentId) public view returns(string,string,string,string){
-        Document storage document = documents[_documentId];
-        return(document.ownerName,document.ownerEmail,document.docTimestamp,document.docHash);
+    function fetchDocumentDetails(string _docHash) public view returns(string,string,string){
+        
+        require( 
+            bytes(_docHash).length > 0 && bytes(_docHash).length <= 256, 
+            "Hash length should be 256 bytes"
+            );
+           
+        Document storage document = documents[_docHash];
+        return(document.docHash,document.docTimestamp,document.currentOwnerName);
     }
 
 }
+    
+    
