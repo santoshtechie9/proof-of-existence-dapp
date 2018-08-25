be4f0f2bf8730af205caf86afd86d8bc3335b604
santoshtechie9@gmail.com <September*09>
2018-08-25 12:30:25 -0700

    updated contract code

---
+++ avoiding_common_attacks.md
@@ -0,0 +1,10 @@
+# Avoiding Common Attacks
+
+| Avoiding Known Attacks | Contract Name |
+| ------------------------------ | ------------------- |
+| `reentrancy` | Proof.sol |
+| `Cross-function Race Conditions` |  Proof.sol |
+| `Timestamp Dependence` |  Proof.sol, ProofDB.sol |
+| `Integer Overflow and Underflow` |  Proof.sol |
+| `DoS with (Unexpected) revert` |  Proof.sol, ProofDB.sol |
+| `DoS with Block Gas Limit` |  Proof.sol, ProofDB.sol |
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,12 +2,8 @@ var Mortal = artifacts.require("./Mortal.sol");
 var ProofDB = artifacts.require("./ProofDB.sol");
 var Proof = artifacts.require("./Proof.sol");
 var Register = artifacts.require("./Register.sol");
-var ProofOfOwnership = artifacts.require("./ProofOfOwnership.sol");
-var ProofOfExistance = artifacts.require("./ProofOfExistance.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(ProofOfOwnership);
-  deployer.deploy(ProofOfExistance);
 
   deployer.deploy(Mortal).then(() => {
     return deployer.deploy(ProofDB)
